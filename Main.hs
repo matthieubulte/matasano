18d1b9c027594e348e9e4680b4e04c3f3d58b561
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2015-05-09 15:06:29 +0200

    Progress on challenge 6

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Data.Bits              as BI
@@ -5,28 +6,66 @@ import qualified Data.ByteString        as B
 import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Char8  as BC
+import qualified Data.Char              as C
 import           Data.Function
 import           Data.Functor
 import qualified Data.List              as L
+import           Numeric                (showHex)
 
-third :: (a, b, c) -> c
-third (_, _, x) = x
+first :: (a, b, c) -> a
+first (x, _, _) = x
 
 second :: (a, b, c) -> b
 second (_, x, _) = x
 
-letters :: String
-letters = ['A'..'z'] ++ ['0'..'9']
+third :: (a, b, c) -> c
+third (_, _, x) = x
 
--- http://home.ccil.org/~cowan/trigrams
-trigrams :: [String]
-trigrams = [ " th", "the", "he ", "to "
-           , " s ", "ed ", " of", " an"
-           , " of", "nd ", " in", "and"
-           ]
+mapSnd :: (b -> c) -> (a, b) -> (a, c)
+mapSnd f (x, y) = (x, f y)
+
+letters :: String
+letters = filter C.isPrint [C.chr 0 .. C.chr 255]
+
+orderedFreqEnglishLetters :: [Char]
+orderedFreqEnglishLetters = " etaoinhsrdlumcwgfyp,.bkv\"\\'-?xj;!qz'"
+
+toHex :: B.ByteString -> String
+toHex = concatMap (`showHex` "") . B.unpack
 
 toB16 :: B.ByteString -> B.ByteString
 toB16 = fst . B16.decode
+
+stringToBits :: String -> [Bool]
+stringToBits = concatMap charToBits
+
+charToBits :: Char -> [Bool]
+charToBits c = BI.testBit (C.ord c) <$> [0..8]
+
+getChunk :: Int -> Int -> [a] -> [a]
+getChunk l o =  take l . drop o
+
+chunk :: Int -> String -> [String]
+chunk l s = L.takeWhile (not . L.null) chunks
+    where
+        offsets = iterate (l +) 0
+        chunks  = ($ s) <$> (getChunk l) <$> offsets
+
+getBsChunk :: Int -> Int -> B.ByteString -> B.ByteString
+getBsChunk l o = B.take l . B.drop o
+
+bsChunk :: Int -> B.ByteString -> [B.ByteString]
+bsChunk l s = L.takeWhile (not . B.null) chunks
+    where
+        offsets = iterate (l +) 0
+        chunks  = ($ s) <$> (getBsChunk l) <$> offsets
+
+
+intDiv :: Int -> Int -> Float
+intDiv x y = (fromIntegral x) / (fromIntegral y)
+
+toPair :: a -> (a, a)
+toPair x = (x, x)
 
 xor :: B.ByteString -> B.ByteString -> B.ByteString
 xor a b = B.pack $ B.zipWith BI.xor a b
@@ -39,15 +78,6 @@ xorN k s = xor s (BC.pack key)
     where
         key = take (B.length s) (cycle k)
 
-trigramsOf :: String -> [String]
-trigramsOf []        = []
-trigramsOf [x]       = [[x, ' ', ' ']]
-trigramsOf [x, y]    = [[x, y, ' ']]
-trigramsOf (x:(y:(z:r))) = [x, y, z] : trigramsOf (y:z:r)
-
-score :: String -> Int
-score s = length $ L.filter id $ (`elem` trigrams) <$> trigramsOf s
-
 best1Xor :: B.ByteString -> (Char, B.ByteString, Int)
 best1Xor s = last result
     where
@@ -55,6 +85,23 @@ best1Xor s = last result
         xorToLetter    = L.zip letters xors
         unsortedResult = (\(letter, str) -> (letter, str, score $ BC.unpack str)) <$> xorToLetter
         result         = L.sortBy (compare `on` third) unsortedResult
+
+histogram :: String -> [(Char, Int)]
+histogram s = L.zip (L.head <$> ls) (L.length <$> ls)
+    where
+        ls = L.group $ L.sort s
+
+rankedLetters :: String -> [Char]
+rankedLetters s = L.reverse $ fst <$> L.sortBy (compare `on` snd) (histogram s)
+
+score :: String -> Int
+score s = (*) (-1) $ L.sum $ distance <$> indexedRank
+    where
+        localRank = rankedLetters s
+        indexedRank = [0, 1..] `L.zip` localRank
+        distance (i, c) = case (L.elemIndex c orderedFreqEnglishLetters) of
+            Nothing -> L.length s
+            Just i' -> abs (i - i')
 
 -- Challenge 1
 challenge1 :: Bool
@@ -95,6 +142,53 @@ challenge5 = xored == expected
         key      = "ICE"
         xored    = key `xorN` original
 
+-- Challenge 6
+
+challenge6 :: IO [([Char], BC.ByteString)]
+challenge6 = do
+    contentB64          <- readFile "challenge6.txt"
+
+    let (Right content) = B64.decode (BC.pack contentB64)
+    let strContent      = BC.unpack content
+
+    let probableKeyLengths = bestKeyLengths 5 strContent
+
+    -- for each key length L, split the content in chunks of size L
+    let chunks             = (`bsChunk` content) <$> probableKeyLengths
+
+    -- transpose the chunks
+    let transposed         = B.transpose <$> chunks
+
+    -- for each chunk, get the best single xor character
+    let keyPerBlock        = (fmap . fmap) best1Xor transposed
+
+    print $ (fmap . fmap) (\(k, _, s) -> (k, s)) keyPerBlock
+
+    -- join the best xor character of each block into the xor key
+    let keys               = (fmap . fmap) first keyPerBlock
+
+    let pretty             = (mapSnd $ (`xorN` content)) <$> toPair <$> keys
+
+    return pretty
+
+bestKeyLengths :: Int -> String -> [Int]
+bestKeyLengths m s = fst <$> take m scoredLengths
+    where
+        lengthToScore = (mapSnd $ scoreKeyLength s) <$> toPair <$> [2..40]
+        scoredLengths = L.sortBy (compare `on` snd) lengthToScore
+
+scoreKeyLength :: String -> Int -> Float
+scoreKeyLength s l = (hamming chunk1 chunk2) `intDiv` l
+    where
+        chunk1 = getChunk (l - 1) 0 s
+        chunk2 = getChunk (2 * l) l s
+
+hamming :: String -> String -> Int
+hamming s s' = length . filter (not . id) $ L.zipWith (==) bs bs'
+    where
+        bs  = stringToBits s
+        bs' = stringToBits s'
+
 main :: IO ()
 main = do
     putStr "Challenge 1: "
@@ -111,4 +205,7 @@ main = do
 
     putStr "Challenge 5: "
     print challenge5
+
+    putStr "Challenge 6: "
+    challenge6 >>= print
 
